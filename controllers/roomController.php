a611c0d163e62195403098cde773b3f0a7bf7a54
shimaaFathi <shimaafathi126@gmail.com>
2024-04-27 15:29:13 +0300

    add-Get-Rooms

--- controllers/roomController.php
+++ controllers/roomController.php
@@ -4,21 +4,34 @@ require_once __DIR__ . '/../models/Room.php';
 
 class RoomController
 {
-    private $room;
+    private $conn;
+    private $room; // Add this property
 
     public function __construct(PDO $conn)
     {
-        $this->room = new Room($conn);
+        $this->conn = $conn;
+        $this->room = new Room($conn); // Initialize the Room object
     }
 
     public function create($name)
     {
-        if ($this->room->createRoom($name)) {
+        if ($this->room->createRoom($name)) { // Access the createRoom() method via $this->room
             return true;
         } else {
             return false;
         }
     }
-}
 
+    public function getAllRooms()
+    {
+        $query = "SELECT * FROM rooms";
+        $stmt = $this->conn->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getRoomByName($name)
+    {
+        return $this->room->getRoomByName($name);
+    }
+}
 ?>
