30713e10a666563fcc0a354ea24822bf74681555
shimaaFathi <shimaafathi126@gmail.com>
2024-04-30 00:53:01 +0300

    send-email-reset-pass

--- views/admin/reset_password.php
+++ views/admin/reset_password.php
@@ -1,5 +1,9 @@
 <?php
 require_once __DIR__ . '/../../utilities/db_connection.php';
+require_once __DIR__ . '/../../vendor/autoload.php'; // Include Composer's autoloader
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 $message = '';
 
@@ -21,18 +25,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $email_exists = $check_stmt->fetchColumn();
             
             if ($email_exists) {
-                // Email exists, proceed with password update
+                // Send email to user
+                $mail = new PHPMailer(true);
+
+                //Server settings
+                $mail->isSMTP();
+                $mail->Host       = 'smtp.gmail.com'; // SMTP server
+                $mail->SMTPAuth   = true;
+                $mail->Username   = 'fathishimaa218@gmail.com'; // SMTP username
+                $mail->Password   = 'dlxs wwvk evms inna'; // SMTP password
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port       = 465;
+
+                //Recipients
+                $mail->setFrom('fathishimaa218@gmail.com', 'cafe');
+                $mail->addAddress($_POST['email']); // User's email
+
+                // Content
+                $mail->isHTML(true);
+                $mail->Subject = 'Reset Password';
+                $mail->Body    = 'Please click the following link to reset your password: <a href="http://example.com/reset_password.php">Reset Password</a>';
+
+                $mail->send();
+
+                // Update password
                 $update_stmt = $pdo->prepare("UPDATE users SET password = :new_password WHERE email = :email");
                 $update_stmt->bindParam(':new_password', $_POST['new_password']);
                 $update_stmt->bindParam(':email', $_POST['email']);
                 $update_stmt->execute();
-                $message = "Password updated successfully!";
+
+                $message = "Password updated successfully! Please check your email for further instructions.";
             } else {
                 // Email does not exist
                 $message = "Email does not exist!";
             }
         } catch(PDOException $e) {
             $message = "Error updating password: " . $e->getMessage();
+        } catch (Exception $e) {
+            $message = "Error sending email: " . $mail->ErrorInfo;
         }
     } else {
         $message = "Passwords do not match!";
