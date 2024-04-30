5d1da844ee1c35a986315c7532c70cb88ffa7ab3
shimaaFathi <shimaafathi126@gmail.com>
2024-04-30 17:47:33 +0300

    update-reset-after-email

--- views/admin/reset-password/reset-after-email.php
+++ views/admin/reset-password/reset-after-email.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../../utilities/db_connection.php';
-require_once __DIR__ . '/../../../vendor/autoload.php'; // Include Composer's autoloader
+require_once __DIR__ . '/../../../vendor/autoload.php'; 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -13,11 +13,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../../');
             $dotenv->load();
 
-            $pdo = new PDO($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
             // Update password
-            $update_stmt = $pdo->prepare("UPDATE users SET password = :new_password WHERE email = :email");
+            $update_stmt = $conn->prepare("UPDATE users SET password = :new_password WHERE email = :email");
             $update_stmt->bindParam(':new_password', $_POST['new_password']);
             $update_stmt->bindParam(':email', $_POST['email']); // Assuming you have stored the user's email somewhere
             $update_stmt->execute();
